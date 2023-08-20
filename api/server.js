1d9e926aa35fc835906f1c974c401572bfad037d
Ben Johnson <monk.sound.design@gmail.com>
2023-08-20 12:22:05 -0400

    error messages

--- api/server.js
+++ api/server.js
@@ -10,13 +10,17 @@ server.post("/api/users", async (req, res) => {
   const { name, bio } = req.body;
   try {
     if (!name || !bio) {
-      res.status(422).json({ message: "Please include a name and bio" });
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
     } else {
       const newUser = await Model.insert({ name, bio });
       res.status(201).json(newUser);
     }
   } catch (err) {
-    res.status(500).json(`Error: ${err.message}`);
+    res.status(500).json({
+      message: "There was an error while saving the user to the database",
+    });
   }
 });
 
@@ -25,7 +29,9 @@ server.get("/api/users", async (req, res) => {
     const users = await Model.find();
     res.status(200).json(users);
   } catch (err) {
-    res.status(500).json(`Error: ${err.message}`);
+    res
+      .status(500)
+      .json({ message: "The users information could not be retrieved" });
   }
 });
 
@@ -34,12 +40,16 @@ server.get("/api/users/:id", async (req, res) => {
   const user = await Model.findById(id);
   try {
     if (!user) {
-      res.status(404).json(`Could not find user with id ${id}`);
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
     } else {
       res.status(200).json(user);
     }
   } catch (err) {
-    res.status(500).json(`Error: ${err.message}`);
+    res
+      .status(500)
+      .json({ message: "The user information could not be retrieved" });
   }
 });
 
@@ -48,12 +58,14 @@ server.delete("/api/users/:id", async (req, res) => {
   const deletedUser = await Model.remove(id);
   try {
     if (!deletedUser) {
-      res.status(404).json(`Could not find user with id ${id}`);
+      res
+        .status(404)
+        .json({ message: "The user with the specified ID does not exist" });
     } else {
       res.status(200).json(deletedUser);
     }
   } catch (err) {
-    res.status(500).json(`Error: ${err.message}`);
+    res.status(500).json({ message: "The user could not be removed" });
   }
 });
 
@@ -62,17 +74,23 @@ server.put("/api/users/:id", async (req, res) => {
   const { name, bio } = req.body;
   try {
     if (!name || !bio) {
-      res.status(422).json("Please include a name and bio");
+      res
+        .status(400)
+        .json({ message: "Please provide name and bio for the user" });
     } else {
       const updatedUser = await Model.update(id, { name, bio });
       if (!updatedUser) {
-        res.status(404).json(`Could not find user with id ${id}`);
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
       } else {
         res.status(200).json(updatedUser);
       }
     }
   } catch (err) {
-    res.status(500).json(`Error: ${err.message}`);
+    res
+      .status(500)
+      .json({ message: "The user information could not be modified" });
   }
 });
 
